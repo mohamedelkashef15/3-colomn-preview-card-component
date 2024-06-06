5e6af4da89aa8f8f4e3a49ccfc0897467900a0e5
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2024-06-07 00:28:44 +0300

    update images

--- src/App.tsx
+++ src/App.tsx
@@ -10,19 +10,19 @@ function App() {
     <main>
       <div className="container">
         <Card
-          image="/images/icon-sedans.svg"
+          image="images/icon-sedans.svg"
           name="SEDANS"
           description="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip."
           bgColor="hsl(31, 77%, 52%)"
         />
         <Card
-          image="/images/icon-suvs.svg"
+          image="images/icon-suvs.svg"
           name="SUVS"
           description="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
           bgColor="hsl(184, 100%, 22%)"
         />
         <Card
-          image="./images/icon-luxury.svg"
+          image="images/icon-luxury.svg"
           name="LUXURY"
           description="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style."
           bgColor="hsl(179, 100%, 13%)"
